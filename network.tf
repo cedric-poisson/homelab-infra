9dc240a523f6b83f5ecce8ea60e51c4decd3bb27
Cedric <poisson.cedric09@gmail.com>
2026-08-22 10:15:51 +0200

    feat: add vnet and subnet

---
+++ network.tf
@@ -0,0 +1,13 @@
+resource "azurerm_virtual_network" "homelab" {
+  name                = "vnet-homelab-dev"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.homelab.location
+  resource_group_name = azurerm_resource_group.homelab.name
+}
+
+resource "azurerm_subnet" "vms" {
+  name                 = "snet-vms"
+  resource_group_name  = azurerm_resource_group.homelab.name
+  virtual_network_name = azurerm_virtual_network.homelab.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
